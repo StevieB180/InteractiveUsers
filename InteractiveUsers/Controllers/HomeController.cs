517d3776f31670da7dbe3668322b867d13bb3bef
Stephen Brett <S00173498@mail.itsligo.ie>
2019-06-23 14:06:26 +0100

    Application runs

--- InteractiveUsers/Controllers/HomeController.cs
+++ InteractiveUsers/Controllers/HomeController.cs
@@ -1,30 +1,113 @@
-﻿using System;
+﻿using InteractiveUsers.Models;
+using Shapeclasses;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace InteractiveUsers.Controllers
+namespace DraggableInteractiveDivs.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private BoardContext db = new BoardContext();
+        // GET: Home
+        public ActionResult Index(int? id, string type)
+        {
+            if (type == "Edit")
+            {
+                var cardProp = db.Cards.Where(n => n.CardID == id.Value).FirstOrDefault();
+                ViewBag.CardId = cardProp.CardID;
+                ViewBag.CardTitle = cardProp.Title;
+                ViewBag.CardText = cardProp.Text;
+            }
+
+            //ViewBag.CategoryList = getCategoryList();
+            ViewBag.CardsList = getCardList();
+            return View(db.Cards);
+        }
+
+        public ActionResult SecondApproach()
         {
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+        //[HttpPost]
+        //public ActionResult UpdateCard(Card card)
+        //{
+        //    using (BoardContext db = new BoardContext())
+        //    {
+        //        Card updatedCard = (from c in db.Cards
+        //                            where c.CardID == card.CardID
+        //                            select c).FirstOrDefault();
+        //        updatedCard.Title = card.Title;
+        //        updatedCard.Text = card.Text;
+        //        //updatedCard.CategoryName = card.CategoryName;
+        //        db.SaveChanges();
+        //    }
+        //    return RedirectToAction("Index");
+        //}
 
-            return View();
+        [HttpPost]
+        public ActionResult AddCardSignal(Card card)
+        {
+
+            if (ModelState.IsValid)
+            {
+
+                db.Cards.Add(card);
+                db.SaveChanges();
+
+                return Json(card.CardID, JsonRequestBehavior.AllowGet);
+            }
+            return Json(null, JsonRequestBehavior.AllowGet);
+
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
 
-            return View();
+        [HttpPost]
+        public ActionResult Index([Bind(Include = "CategoryName,Title,Text,Vote")]
+                ViewModel viewView, Card card)
+        {
+            if (ModelState.IsValid)
+            {
+                // Create a new Card
+                Card newCard = new Card
+                {
+
+                    Title = viewView.Title,
+                    Text = viewView.Text
+                };
+
+                db.Cards.Add(newCard);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Message = "N/A";
+            //ViewBag.CategoryList = getCategoryList();
+            ViewBag.CardsList = getCardList();
+
+            return View(viewView);
         }
+
+        //aoife start
+        public List<Card> getCardList()
+        {
+            List<Card> card = new List<Card>();
+            //getting cards in db
+            card = db.Cards.ToList();
+
+            var cards = new List<Card>();
+            foreach (var item in card)
+            {
+                if (item.Title != null || item.Text != null)
+                    cards.Add(item);
+            }
+            return cards;
+        }
+
+
     }
 }
